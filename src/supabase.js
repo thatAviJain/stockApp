9aeb427ee141326b5a120ec2a33e53e336921630
Avi Jain <avijain171@gmail.com>
2025-09-02 14:55:51 +0530

    Finishing Touches

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,7 @@
 // src/supabase.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ysqjhsyuwlikldkanpfj.supabase.co";
-const supabaseAnonKey =
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzcWpoc3l1d2xpa2xka2FucGZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3MDQwNjUsImV4cCI6MjA2NzI4MDA2NX0.N1ZaHeRj_RHkdv_av8pYNgpgyMcvxN6cSFYTRtyyr-A";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
